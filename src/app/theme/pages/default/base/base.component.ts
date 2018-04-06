5da3d2ea56966521dad5b1e744981b73219f95ae
jorgeyosmiel90@gmail.com <jorgeyosmiel90@gmail.com>
2018-04-05 21:23:55 -0400

    adding base component to manage error and use toaster

---
+++ src/app/theme/pages/default/base/base.component.ts
@@ -0,0 +1,93 @@
+import {OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Response} from "@angular/http";
+declare var toastr: any;
+declare var mApp: any;
+
+export class BaseComponent implements OnInit {
+
+	constructor(protected _router: Router) {
+
+	}
+
+	ngOnInit() {
+
+	}
+
+	onError( response: Response)
+	{
+		if ( response.ok )
+		{
+			let data = response.json();
+			if ( data.code === 403 )
+			{
+				this.showError(data.error);
+				this._router.navigate(['logout']);
+			}
+			else
+			{
+				this.showError(data.error);
+			}
+		}
+		else {
+			this.showError("An error occurred on the server.");
+		}
+
+	}
+
+	showError(message)
+	{
+		this.getToastr().error(message);
+	}
+
+	showSuccess(message)
+	{
+		this.getToastr().success(message);
+	}
+
+	showInfo(message)
+	{
+		this.getToastr().info(message);
+	}
+
+	showWarning(message)
+	{
+		this.getToastr().warning(message);
+	}
+
+	getToastr()
+	{
+		toastr.options = {
+			"closeButton": false,
+			"debug": false,
+			"newestOnTop": false,
+			"progressBar": true,
+			"positionClass": "toast-top-right",
+			"preventDuplicates": true,
+			"onclick": null,
+			"showDuration": "300",
+			"hideDuration": "1000",
+			"timeOut": "5000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+			"showMethod": "fadeIn",
+			"hideMethod": "fadeOut"
+		};
+
+		return toastr;
+	}
+
+	block(enable)
+	{
+		if ( enable)
+		{
+			mApp.block('.m-page--fluid', {message:"Please wait..."});
+		}
+		else
+		{
+			mApp.unblock('.m-page--fluid');
+		}
+
+	}
+}
